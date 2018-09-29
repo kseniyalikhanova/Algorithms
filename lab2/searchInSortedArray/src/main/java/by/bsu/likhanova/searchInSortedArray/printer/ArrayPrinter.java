57ede40ad9a577185cd939c1f0264b7798cc745e
k.likhanova <kseniya.likhanova@gmail.com>
2018-09-29 18:29:39 +0300

    Create the project SearchInSortedArray.

---
+++ lab2/searchInSortedArray/src/main/java/by/bsu/likhanova/searchInSortedArray/printer/ArrayPrinter.java
@@ -0,0 +1,10 @@
+package by.bsu.likhanova.searchInSortedArray.printer;
+
+public class ArrayPrinter {
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ lab2/searchInSortedArray/src/main/java/by/bsu/likhanova/searchInSortedArray/runner/SearchRunner.java
@@ -0,0 +1,7 @@
+package by.bsu.likhanova.searchInSortedArray.runner;
+
+public class SearchRunner {
+    public static void main(String[] args) {
+
+    }
+}
