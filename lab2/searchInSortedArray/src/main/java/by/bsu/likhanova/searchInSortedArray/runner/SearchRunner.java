9e83cc8ea4fccab759832b8249fd9b98b3670716
k.likhanova <kseniya.likhanova@gmail.com>
2018-09-29 21:10:56 +0300

    Search time.

--- lab2/searchInSortedArray/src/main/java/by/bsu/likhanova/searchInSortedArray/runner/SearchRunner.java
+++ lab2/searchInSortedArray/src/main/java/by/bsu/likhanova/searchInSortedArray/runner/SearchRunner.java
@@ -1,7 +1,18 @@
 package by.bsu.likhanova.searchInSortedArray.runner;
 
+import by.bsu.likhanova.searchInSortedArray.search.BinarySearch;
+import by.bsu.likhanova.searchInSortedArray.search.InterpolationSearch;
+
 public class SearchRunner {
     public static void main(String[] args) {
-
+        int[] array = {1,3,5,7,9,11,13,15,17,19,21,23,25,27,29};
+        long start = System.nanoTime();
+        System.out.println(BinarySearch.runBinarySearch(array,23));
+        long finish = System.nanoTime();
+        System.out.println("Binary search with " + array.length + " elements: " + String.format("%,12d", (finish-start)) + " ns;");
+        start = System.nanoTime();
+        System.out.println(InterpolationSearch.runInterpolationSearch(array,23));
+        finish = System.nanoTime();
+        System.out.println("Interpolation search with " + array.length + " elements: " + String.format("%,12d", (finish-start)) + " ns;");
     }
 }
---
+++ lab2/searchInSortedArray/src/main/java/by/bsu/likhanova/searchInSortedArray/search/BinarySearch.java
@@ -0,0 +1,28 @@
+package by.bsu.likhanova.searchInSortedArray.search;
+
+public class BinarySearch {
+
+    public static int runBinarySearch(int[] array, int key) {
+        int index = -1;
+        int left = 0;
+        int right = array.length - 1;
+        int middle;
+        while (array[left] < key && array[right] > key) {
+            middle = left + (right - left) / 2;
+            if (key == array[middle]) {
+                index = middle;
+                break;
+            } else if (key < array[middle]) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+        if (key == array[left]) {
+            index = left;
+        } else if (key == array[right]) {
+            index = right;
+        }
+        return index;
+    }
+}
