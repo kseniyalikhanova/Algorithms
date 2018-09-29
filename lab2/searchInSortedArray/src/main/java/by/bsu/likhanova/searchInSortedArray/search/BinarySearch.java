29a0c8d89ccd54ed0924987f03196b080265e24f
k.likhanova <kseniya.likhanova@gmail.com>
2018-09-29 19:28:04 +0300

    Rewriting Binary search.

--- lab2/searchInSortedArray/src/main/java/by/bsu/likhanova/searchInSortedArray/search/BinarySearch.java
+++ lab2/searchInSortedArray/src/main/java/by/bsu/likhanova/searchInSortedArray/search/BinarySearch.java
@@ -1,17 +1,22 @@
 package by.bsu.likhanova.searchInSortedArray.search;
 
 public class BinarySearch {
-    public static int runBinarySearch(int[] array, int key, int left, int right){
-        int middle = left + (right-left)/2;
-        if (right < left) {
-            return -1;
+    public static int runBinarySearch(int[] array, int key){
+        int index = -1;
+        int left = 0, right = array.length, mid;
+        while (left < right) {
+            mid = left + (right-left)/2;
+            if (key == array[mid]) {
+                index = mid;
+                break;
+            } else {
+                if (key <= array[mid]) {
+                    right = mid-1;
+                } else {
+                    left = mid + 1;
+                }
+            }
         }
-        if (key == array[middle]) {
-            return middle;
-        } else if (key < array[middle]) {
-            return runBinarySearch(array, key, left, middle - 1);
-        } else {
-            return runBinarySearch(array, key, middle + 1, right);
-        }
+        return index;
     }
 }
