7a6d41872d698bca8d3f6baeb7c88641c1a408b7
k.likhanova <kseniya.likhanova@gmail.com>
2018-11-23 20:00:45 +0300

    Change name of class

--- lab6/Dijkctra/src/main/java/by/bsu/likhanova/runner/GraphRunner.java
+++ lab6/Dijkctra/src/main/java/by/bsu/likhanova/runner/GraphRunner.java
@@ -3,7 +3,7 @@ package by.bsu.likhanova.runner;
 import by.bsu.likhanova.entity.GraphWithAdjacencyList;
 import by.bsu.likhanova.menu.WorkWithGraph;
 
-public class UndirectedGraphRunner {
+public class GraphRunner {
     public static void main(String[] args) {
         WorkWithGraph.printMenu(new GraphWithAdjacencyList());
     }
