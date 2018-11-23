572bbee057a06a719d6d5dcd077fe9bbc682b650
k.likhanova <kseniya.likhanova@gmail.com>
2018-11-23 18:36:31 +0300

    Create Floyd's algorithm.

--- lab6/Floyd/src/main/java/by/bsu/likhanova/algorithm/Floyd.java
+++ lab6/Floyd/src/main/java/by/bsu/likhanova/algorithm/Floyd.java
@@ -2,8 +2,26 @@ package by.bsu.likhanova.algorithm;
 
 import by.bsu.likhanova.entity.GraphWithAdjacencyMatrix;
 
+import static java.lang.Integer.min;
+
 public class Floyd {
-    public static GraphWithAdjacencyMatrix perform(final GraphWithAdjacencyMatrix graph){
-        GraphWithAdjacencyMatrix resultGraph = new GraphWithAdjacencyMatrix(graph.)
+    private static final int INFINITY = Integer.MAX_VALUE;
+
+    public static GraphWithAdjacencyMatrix perform(final GraphWithAdjacencyMatrix graph) {
+        GraphWithAdjacencyMatrix resultGraph = graph;
+        for (int k = 0; k < graph.getSize(); ++k) {
+            for (int i = 0; i < graph.getSize(); ++i) {
+                for (int j = 0; j < graph.getSize(); ++j) {
+                    if (resultGraph.getAdjacencyMatrix()[i][k] < INFINITY
+                            && resultGraph.getAdjacencyMatrix()[k][j] < INFINITY) {
+                        resultGraph.getAdjacencyMatrix()[i][j] =
+                                min(resultGraph.getAdjacencyMatrix()[i][j],
+                                  resultGraph.getAdjacencyMatrix()[i][k]
+                                      + resultGraph.getAdjacencyMatrix()[k][j]);
+                    }
+                }
+            }
+        }
+        return resultGraph;
     }
 }
